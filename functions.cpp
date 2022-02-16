b7fe389d62bf8bde70dd5ba16492ffe978d0b064
wwhammelACC <89032402+wwhammelACC@users.noreply.github.com>
2022-02-16 12:23:06 -0600

    pushing original but with some commented out and some added code to test commented out

---
+++ functions.cpp
@@ -0,0 +1,56 @@
+/*
+ * the supporting demo functions implementations
+ */
+
+#include "functions.h"
+
+// pass in a pointer to a Data
+bool change_data(Data *data_struct){
+    // change the values stored
+    data_struct->id = 2;
+    data_struct->data = "Good Bye Cruel World";
+    
+    // return true to indicate success
+    return true;
+}
+
+// pass in an array of type Data and the size of the array
+//bool fill_data_array(Data *array, int size){
+//    for(int i=0; i<size; i++){
+//        // fill with ids 10,20,etc. and a generic string
+//        array[i].id = (i+1)*10;
+//        array[i].data =  "some string " + std::to_string(i+1);
+//    }
+//    return true;
+//}
+//
+// pass in an array of type Data and the size of the array
+//void display_data_array(Data *array, int size){
+//    for(int i=0; i<size; i++){
+//        cout << array[i].id << ": " << array[i].data << endl;
+//    }
+//    cout << endl;
+//}
+
+// 2d array version
+// pass in an array of type Data and the size of the array
+//bool fill_data_array(Data **array, int rows, int cols){
+//    for(int i=0; i<rows; i++){
+//        // fill with ids 10,20,etc. and a generic string
+//        for (int j = 0; j < cols; j++) {
+//            array[i]->id = (i+1)*10;
+//            array[i]->data =  "some string " + std::to_string(i+1);
+//        }
+//    }
+//    return true;
+//}
+//
+////pass in an array of type Data and the size of the array
+//void display_data_array(Data **array, int rows, int cols){
+//    for(int i=0; i<rows; i++){
+//        for (int j = 0; j < cols; j++) {
+//            cout << array[i]->id << ": " << array[i]->data << endl;
+//        }
+//    }
+//    cout << endl;
+//}
